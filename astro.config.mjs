d2fc61fb5d2a80985268310cba912ecc87c9fdd0
Alex Leung <alexleung25@outlook.com>
2023-10-31 17:31:54 -0700

    Update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    markdown: {
+        syntaxHighlight: 'shiki',
+        shikiConfig: {
+            theme: 'material-theme-lighter'
+        }
+    }
+});
